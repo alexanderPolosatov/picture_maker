25bf706bf6dc59c1bb97cd76565e1a22313263ad
alexander <registrds@yandex.ru>
2024-11-30 23:57:04 +0300

    upd

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,8 @@ export class PictureMaker {
         private shootingTime: number,
         private timeout: number,
         private address: string,
-        private windowSize: { width: number, height: number },
-        private PHOTO_FOLDER: string,
+        private resultFolder: string,
+        private windowSize?: { width: number, height: number },
         private logAction: (action: string) => void = () => null
     ) { }
 
@@ -20,7 +20,7 @@ export class PictureMaker {
             const browserData = await this.createBrowserData();
             const countOfShots = this.shootingTime * 60 / (this.timeout / 1000);
 
-            createFolderRecursiveIfNoExist(this.PHOTO_FOLDER);
+            createFolderRecursiveIfNoExist(this.resultFolder);
 
             await this.takeScreenShots(browserData.page, countOfShots, this.timeout);
 
@@ -38,15 +38,17 @@ export class PictureMaker {
         this.logAction('End');
     }
 
-    async createBrowserData() {
-        const { width = 1450, height = 720 } = this.windowSize
+    private async createBrowserData() {
         const browser = await puppeteer.launch({ args: ['--disable-features=site-per-process'] });
 
         const page = await browser.newPage();
 
         page.setDefaultNavigationTimeout(0);
 
-        await page.setViewport({ width, height });
+        await page.setViewport({
+            width: this.windowSize?.width ?? 1450,
+            height: this.windowSize?.height ?? 720
+        });
         page.goto(this.address, { waitUntil: 'networkidle0' });
 
         await wait(5000);
@@ -54,10 +56,10 @@ export class PictureMaker {
         return { browser, page };
     }
 
-    async takeScreenShots(page: Page, screenshotsCount: number, timeoutToWait: number) {
+    private async takeScreenShots(page: Page, screenshotsCount: number, timeoutToWait: number) {
         for (let i = 0; i < screenshotsCount; i++) {
             const data = await page.screenshot({ type: 'png' });
-            const fileName = join(this.PHOTO_FOLDER, `${getDateString(new Date())}.png`);
+            const fileName = join(this.resultFolder, `${getDateString(new Date())}.png`);
 
             writeFileSync(fileName, data);
             const action = `${i + 1} screenshot of ${screenshotsCount}`;
